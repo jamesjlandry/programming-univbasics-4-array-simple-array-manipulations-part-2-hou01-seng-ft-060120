b4834c8672011be9450226db729c8290d2076c6e
James Landry <james.j.landry@outlook.com>
2020-05-19 17:43:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,9 +15,9 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  array.delete_at(string)
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
-  array.delete(2)
+  array.delete_at(2)
 end
